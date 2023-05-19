24afcd312292beb676f54ab68593c72ad67311f3
Avery <averycaldwell7@gmail.com>
2023-05-19 15:12:39 -0700

    edit about styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import * as Material from '@mui/material';
+// import * as Icons from '@mui/icons-material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CreateIcon from '@mui/icons-material/Create';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
 // MUI theme
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
+// IMAGE
+import avesEmote from '../assets/aves_peace.png';
 
 //  Function to render About component
 function About(props) {
@@ -13,92 +19,93 @@ function About(props) {
             {props.currentPage === 'About' && (
                 <ThemeProvider theme={theme}>
                     {/* FIXME : theming */}
-
-                    <Material.Grid
+                    <Material.Box
                         sx={{
-                            p: 5,
-                            mx: 'auto',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'space-evenly',
-                            alignItem: 'center',
-                            position: 'absolute',
-                            flexGrow: 1,
+                            ml: 'auto',
+                            mr: 'auto',
+                            mt: '150px',
                         }}
                     >
-                        <h1>About</h1>
-                        <p>
-                            Welcome to my React portfolio! I'm a full-stack
-                            developer with a marketing degree, blending design
-                            and coding skills. Freshly graduated from a
-                            bootcamp, I'm ready to conquer the digital realm.
-                            When I'm not coding, you'll find me live streaming
-                            on Twitch, sharing my experiences and cracking
-                            jokes. With a marketer's eye, I create visually
-                            stunning and user-friendly applications. Explore my
-                            projects and let's build something amazing together!
-                            🚀
-                        </p>
-                        <p>
-                            Combining marketing with coding offers numerous
-                            benefits that can enhance your effectiveness and
-                            impact in the digital landscape. Here are some key
-                            advantages:
-                        </p>
-                        <p>
-                            Enhanced Design Skills: A solid understanding of
-                            marketing principles allows you to leverage your
-                            coding skills to create visually appealing and
-                            user-friendly digital experiences. You can optimize
-                            the user interface and user experience (UI/UX) to
-                            effectively communicate your brand message and drive
-                            conversions.
-                        </p>
-                        <p>
-                            Data-Driven Decision Making: As a marketer with
-                            coding knowledge, you can leverage data analytics
-                            and tracking tools to gain insights into user
-                            behavior, campaign performance, and customer
-                            preferences. This data-driven approach empowers you
-                            to make informed decisions and refine marketing
-                            strategies for maximum impact.
-                        </p>
-                        <p>
-                            Automation and Efficiency: Coding enables you to
-                            automate repetitive marketing tasks, such as data
-                            collection, analysis, and campaign management. By
-                            building custom tools or utilizing existing ones,
-                            you can streamline workflows, save time, and
-                            increase overall efficiency.
-                        </p>
-                        <p>
-                            Personalized and Dynamic Marketing: With coding
-                            skills, you can create dynamic and personalized
-                            marketing campaigns. By leveraging technologies like
-                            APIs, databases, and user segmentation, you can
-                            deliver tailored content and experiences to your
-                            target audience, increasing engagement and
-                            conversion rates.
-                        </p>
-                        <p>
-                            Adaptability in the Digital Age: The ability to code
-                            provides you with a competitive edge in a rapidly
-                            evolving digital landscape. You can stay ahead by
-                            quickly adapting to new technologies, exploring
-                            emerging platforms, and implementing innovative
-                            marketing strategies that leverage the latest
-                            trends.
-                        </p>
-                        <p>
-                            Cross-Collaboration: Being proficient in both
-                            marketing and coding allows you to bridge the gap
-                            between marketing teams and technical teams. You can
-                            effectively communicate with developers, understand
-                            technical constraints, and translate marketing
-                            objectives into actionable code, fostering seamless
-                            collaboration and efficient project execution.
-                        </p>
-                    </Material.Grid>
+                        <Material.Typography variant='h1' component='h2'>
+                            About
+                            <img
+                                src={avesEmote}
+                                style={{
+                                    height: '250px',
+                                    width: '250px',
+                                    // FIXME: theming
+                                    backgroundColor: 'black',
+                                    borderRadius: 300,
+                                    border: '2px solid #e9f4e9',
+                                    boxShadow: 5,
+                                    cursor: 'pointer',
+                                }}
+                                alt='Aves Emote'
+                                // onClick={() => props.setCurrentPage('Contact')}
+                            />
+                        </Material.Typography>
+                        <Material.Grid container spacing={3}>
+                            <Material.Grid item xl>
+                                <Material.Typography
+                                    variant='body1'
+                                    gutterBottom
+                                >
+                                    I'm Avery. A full-stack developer with a
+                                    marketing degree, blending design and coding
+                                    skills.
+                                </Material.Typography>
+                            </Material.Grid>
+                            <br />
+                            <Material.Grid item m>
+                                {/* FIXME do a .map to cycle through the list of items? */}
+                                <CreateIcon></CreateIcon>
+                                <Material.Typography variant='h5' gutterBottom>
+                                    Designer/Creator
+                                </Material.Typography>
+                                <Material.Typography variant='h6' gutterBottom>
+                                    Frameworks
+                                </Material.Typography>
+                                <Material.Typography variant='h6' gutterBottom>
+                                    Design Tools:
+                                </Material.Typography>
+                            </Material.Grid>
+                            <Material.Grid item m>
+                                <ColorLensIcon></ColorLensIcon>
+                                <Material.Typography variant='h5' gutterBottom>
+                                    Frontend Developer
+                                </Material.Typography>
+                                <Material.Typography variant='h6' gutterBottom>
+                                    Languages:
+                                </Material.Typography>
+                                <Material.Typography variant='h6' gutterBottom>
+                                    Dev Tools:
+                                </Material.Typography>
+                            </Material.Grid>
+                            <Material.Grid item m>
+                                <SettingsIcon></SettingsIcon>
+                                <Material.Typography variant='h5' gutterBottom>
+                                    Backend Developer
+                                </Material.Typography>
+                                <Material.Typography variant='h6' gutterBottom>
+                                    Languages:
+                                </Material.Typography>
+
+                                <Material.Typography variant='h6' gutterBottom>
+                                    Dev Tools:
+                                    <Material.Typography
+                                        variant='body1'
+                                        gutterBottom
+                                    >
+                                        Apollo
+                                    </Material.Typography>
+                                </Material.Typography>
+                            </Material.Grid>
+                        </Material.Grid>
+                        <Material.Typography variant='caption' gutterBottom>
+                            Explore my projects and let's build something
+                            amazing together! 🚀
+                        </Material.Typography>
+                    </Material.Box>
                 </ThemeProvider>
             )}
         </React.Fragment>
